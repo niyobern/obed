3081b7cec1adb6bae122ab2b2e8d281af93b5e90
niyobern <niyobern@outlook.com>
2023-11-13 18:15:53 +0200

    done

--- pages/tests/index.js
+++ pages/tests/index.js
@@ -1,10 +1,20 @@
 import TestGrid from "/components/test/testgrid";
 import TestBanner from "/components/test/testbanner";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function Tests(){
     const [focused, setFocused] = useState(0)
     const [modal, setModal] = useState(-1)
+    const [attempted, setAttempted] = useState([])
+    const router = useRouter()
+    useEffect(()=> {
+        const token = localStorage.getItem("token")
+        axios.get("https://nvb_backend-1-z3745144.deta.app/study/test", { headers : { "Authorization": token}})
+        .then((res) => setAttempted(res.data))
+        .catch( () => router.replace("/login"))
+    })
     const tests = Array.from(Array(20).keys())
     if (modal >= 0){
         return (
@@ -19,9 +29,16 @@ export default function Tests(){
                 <span onClick={() => setFocused(0)} className={`pb-4 w-full border-b-2 cursor-pointer ${focused === 0 ? "border-black": "border-gray-300"}`}>Isuzuma rishya</span>
                 <span onClick={() => setFocused(1)} className={`pb-4 w-full border-b-2 cursor-pointer ${focused === 1 ? "border-black": "border-gray-300"}`}>Ayo nakoze</span>
             </div>
+            {focused === 0 && (
             <div className="py-8 px-4 grid grid-cols-2 gap-2">
                 {tests.map( (item, index) => <TestGrid number={item} key={index} choose={(num) => setModal(num)}/>)}
             </div>
+            )}
+            {focused === 1 && (
+            <div className="py-8 px-4 grid grid-cols-2 gap-2">
+                {attempted.map( (item, index) => <TestGrid number={item} key={index} choose={(num) => setModal(num)}/>)}
+            </div>
+            )}
         </div>
     )
     }
