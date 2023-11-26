09469c209394e1cd7e5c470ffc39c64f5104d8b6
NIYOMUGABO BERNARD <85235653+niyobern@users.noreply.github.com>
2023-11-26 21:41:37 +0000

    feat: Update team component and styles
    Updated team component with new images and changed styles for various elements

--- components/team.js
+++ components/team.js
@@ -7,8 +7,8 @@ export default function TeamComponent(){
             <div className="flex flex-col md:grid grid-cols-3 gap-4 md:gap-12 flex-wrap justify-evenly">                 
            {/* <!-- Team member card --> */}
           <div className="w-full bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
-            <Image height={500} width={500} className="w-full h-full rounded-full px-2 md:px-4" src="/images/2.jpg" alt="Team member photo"></Image>
-            <div className="px-6 py-4 flex justify-center text-center">
+            <Image height={500} width={500} className="w-full h-fit rounded-full px-2 md:px-4" src="/images/1.png" alt="Team member photo"></Image>
+            <div className="px-6 py-4 flex flex-col items-center text-center">
               <div className="font-bold text-xl mb-2"><span className="capitalize">Mushinzimana</span> Obed</div>
               <p className="text-gray-700 text-base">President</p>
             </div>
@@ -18,8 +18,8 @@ export default function TeamComponent(){
           </div>            
            {/* <!-- Team member card --> */}
           <div className="w-full bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
-            <Image height={500} width={500} className="w-full h-full rounded-full px-2 md:px-4" src="/images/3.jpeg" alt="Team member photo"></Image>
-            <div className="px-6 py-4 flex justify-center text-center">
+            <Image height={500} width={500} className="w-full h-fit rounded-full px-2 md:px-4" src="/images/2.jpg" alt="Team member photo"></Image>
+            <div className="px-6 py-4 flex flex-col items-center text-center">
               <div className="font-bold text-xl mb-2"><span className="capitalize">Nuwayo</span> Mateme Claudette</div>
               <p className="text-gray-700 text-base">Project Officer</p>
             </div>
@@ -29,19 +29,21 @@ export default function TeamComponent(){
           </div>   
            {/* <!-- Team member card --> */}
           <div className="w-full bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
-            <Image height={500} width={500} className="w-full h-full rounded-full px-2 md:px-4" src="/images/5.jpeg" alt="Team member photo"></Image>
-            <div className="px-6 py-4 flex justify-center text-center">
+            <Image height={500} width={500} className="w-full h-fit rounded-full px-2 md:px-4" src="/images/3.jpeg" alt="Team member photo"></Image>
+            <div className="px-6 py-4 flex flex-col items-center text-center">
               <div className="font-bold text-xl mb-2"><span className="capitalize">Hasengeniza</span> Emmanuel</div>
               <p className="text-gray-700 text-base">Vice President</p>
             </div>
             <div className="px-6 py-4 flex justify-center text-center">
                     {/* <!-- Social icons --> */}
             </div>
-          </div>            
+          </div>                         
+        </div>
+        <div className="flex justify-center mx-auto w-8/12">
            {/* <!-- Team member card --> */}
-          <div className="w-full bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
-            <Image height={500} width={500} className="w-full h-full rounded-full px-2 md:px-4" src="/images/6.jpeg" alt="Team member photo"></Image>
-            <div className="px-6 py-4 flex justify-center text-center">
+           <div className="w-full bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
+            <Image height={500} width={500} className="w-full h-fit rounded-full px-2 md:px-4" src="/images/5.jpeg" alt="Team member photo"></Image>
+            <div className="px-6 py-4 flex flex-col items-center text-center">
               <div className="font-bold text-xl mb-2"><span className="capitalize">Dufitibyiringiro</span> Sylivie</div>
               <p className="text-gray-700 text-base">Co-founder and CEO</p>
             </div>
@@ -51,15 +53,15 @@ export default function TeamComponent(){
           </div> 
            {/* <!-- Team member card --> */}
            <div className="w-full bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
-            <Image height={500} width={500} className="bg-white w-full h-full rounded-full px-2 md:px-4" src="/images/1.png" alt="Team member photo"></Image>
-            <div className="px-6 py-4 flex justify-center text-center">
+            <Image height={500} width={500} className="bg-white w-full h-fit rounded-full px-2 md:px-4" src="/images/6.jpeg" alt="Team member photo"></Image>
+            <div className="px-6 py-4 flex flex-col items-center text-center">
               <div className="font-bold text-xl mb-2"><span className="capitalize">Ndacyayisaba</span> Eric</div>
               <p className="text-gray-700 text-base">Communication Officer</p>
             </div>
             <div className="px-6 py-4 flex justify-center text-center">
                     {/* <!-- Social icons --> */}
             </div>
-          </div>              
+          </div> 
         </div>
       </div>
     )
