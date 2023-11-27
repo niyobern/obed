a31194fe90b5e490112242ec802047d844b8defb
niyobern <niyobern@outlook.com>
2023-11-27 03:05:12 -0800

    fixed err

--- components/contact.js
+++ components/contact.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import image from "../public/images/unspash.jpg";
+import image from "../public/images/unsplash.jpg";
 
 export default function Contact(){
     return (
