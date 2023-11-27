01c28dd4d6bbbc55b7102d2c80ad146da71b3b61
niyobern <niyobern@outlook.com>
2023-11-26 23:15:17 -0800

    fixed flex

--- components/team.js
+++ components/team.js
@@ -8,55 +8,55 @@ export default function TeamComponent(){
            {/* <!-- Team member card --> */}
           <div className="w-full bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
             <Image height={500} width={500} className="w-full h-full rounded-full px-2 md:px-4" src="/images/2.jpg" alt="Team member photo"></Image>
-            <div className="px-6 py-4 flex justify-center text-center">
+            <div className="px-6 py-4 flex-col flex items-center text-center">
               <div className="font-bold text-xl mb-2"><span className="capitalize">Mushinzimana</span> Obed</div>
               <p className="text-gray-700 text-base">President</p>
             </div>
-            <div className="px-6 py-4 flex justify-center text-center flex justify-center">
+            <div className="px-6 py-4 flex-col flex items-center text-center flex justify-center">
                     {/* <!-- Social icons --> */}
             </div>
           </div>            
            {/* <!-- Team member card --> */}
           <div className="w-full bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
             <Image height={500} width={500} className="w-full h-full rounded-full px-2 md:px-4" src="/images/3.jpeg" alt="Team member photo"></Image>
-            <div className="px-6 py-4 flex justify-center text-center">
+            <div className="px-6 py-4 flex-col flex items-center text-center">
               <div className="font-bold text-xl mb-2"><span className="capitalize">Nuwayo</span> Mateme Claudette</div>
               <p className="text-gray-700 text-base">Project Officer</p>
             </div>
-            <div className="px-6 py-4 flex justify-center text-center flex justify-center">
+            <div className="px-6 py-4 flex-col flex items-center text-center flex justify-center">
                     {/* <!-- Social icons --> */}
             </div>
           </div>   
            {/* <!-- Team member card --> */}
           <div className="w-full bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
             <Image height={500} width={500} className="w-full h-full rounded-full px-2 md:px-4" src="/images/5.jpeg" alt="Team member photo"></Image>
-            <div className="px-6 py-4 flex justify-center text-center">
+            <div className="px-6 py-4 flex-col flex items-center text-center">
               <div className="font-bold text-xl mb-2"><span className="capitalize">Hasengeniza</span> Emmanuel</div>
               <p className="text-gray-700 text-base">Vice President</p>
             </div>
-            <div className="px-6 py-4 flex justify-center text-center flex justify-center">
+            <div className="px-6 py-4 flex-col flex items-center text-center flex justify-center">
                     {/* <!-- Social icons --> */}
             </div>
           </div>            
            {/* <!-- Team member card --> */}
           <div className="w-full bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
             <Image height={500} width={500} className="w-full h-full rounded-full px-2 md:px-4" src="/images/6.jpeg" alt="Team member photo"></Image>
-            <div className="px-6 py-4 flex justify-center text-center">
+            <div className="px-6 py-4 flex-col flex items-center text-center">
               <div className="font-bold text-xl mb-2"><span className="capitalize">Dufitibyiringiro</span> Sylivie</div>
               <p className="text-gray-700 text-base">Co-founder and CEO</p>
             </div>
-            <div className="px-6 py-4 flex justify-center text-center flex justify-center">
+            <div className="px-6 py-4 flex-col flex items-center text-center flex justify-center">
                     {/* <!-- Social icons --> */}
             </div>
           </div> 
            {/* <!-- Team member card --> */}
            <div className="w-full bg-white rounded overflow-hidden shadow-lg md:m-4 flex flex-col justify-between">
             <Image height={500} width={500} className="bg-white w-full h-full rounded-full px-2 md:px-4" src="/images/1.png" alt="Team member photo"></Image>
-            <div className="px-6 py-4 flex justify-center text-center">
+            <div className="px-6 py-4 flex-col flex items-center text-center">
               <div className="font-bold text-xl mb-2"><span className="capitalize">Ndacyayisaba</span> Eric</div>
               <p className="text-gray-700 text-base">Communication Officer</p>
             </div>
-            <div className="px-6 py-4 flex justify-center text-center flex justify-center">
+            <div className="px-6 py-4 flex-col flex items-center text-center flex justify-center">
                     {/* <!-- Social icons --> */}
             </div>
           </div>              
