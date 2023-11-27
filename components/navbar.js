0ac7b35f177f807ea0049345cc143db2b0404add
niyobern <niyobern@outlook.com>
2023-11-27 03:47:15 -0800

    restzed lg

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function NavBar(){
     return (
         <div className="flex justify-between w-full md:w-11/12 lg:w-10/12 mx-auto text-white">
-            <Image src="/images/logo.png" width={24} height={12}/>
+            <Image src="/images/logo.png" width={60} height={12}/>
             <div className="flex gap-20">
                 <div className="flex gap-2 md:gap-4 lg:gap-6 xl:gap-8">
                     <div>Home</div>
