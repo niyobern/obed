4b701b716c9b31a38f47c0dea5c7e9b7b1a5de0f
niyobern <niyobern@outlook.com>
2023-11-27 03:50:19 -0800

    logo

--- components/navbar.js
+++ components/navbar.js
@@ -1,9 +1,10 @@
 import Link from "next/link"
 import Image from "next/image"
+import Logo from "/public/images/logo.png"
 export default function NavBar(){
     return (
         <div className="flex justify-between w-full md:w-11/12 lg:w-10/12 mx-auto text-white">
-            <Image src="/images/logo.png" width={60} height={12}/>
+            <Image src={Logo} alt="image" className='w-16 md:w-24 h-10 md:h-16 lg:w-32 lg:h-24 justify-self-center mx-auto'/>
             <div className="flex gap-20">
                 <div className="flex gap-2 md:gap-4 lg:gap-6 xl:gap-8">
                     <div>Home</div>
