edc7d228831f6b1347259656657d4e3809c4fe99
niyobern <niyobern@outlook.com>
2023-11-27 03:45:22 -0800

    commiting

--- components/navbar.js
+++ components/navbar.js
@@ -1,8 +1,9 @@
 import Link from "next/link"
+import Image from "next/image"
 export default function NavBar(){
     return (
         <div className="flex justify-between w-full md:w-11/12 lg:w-10/12 mx-auto text-white">
-            <div>DOC</div>
+            <Image src="/images/logo.png" width={24} height={12}/>
             <div className="flex gap-20">
                 <div className="flex gap-2 md:gap-4 lg:gap-6 xl:gap-8">
                     <div>Home</div>
