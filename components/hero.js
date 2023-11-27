1eb74448d2678afd7ae6d986e9d6d29770c36004
niyobern <niyobern@outlook.com>
2023-11-27 02:52:33 -0800

    tailwind

--- components/hero.js
+++ components/hero.js
@@ -9,7 +9,7 @@ export default function Hero(){
                 <div className="col-span-2 flex flex-col w-full px-20 py-8">
                     <div className="text-5xl font-medium leading-relaxed">Join the <span className="text-green-600">Restore Health Initiatives</span> <br/> for your <br/> <span className="text-amber-400">Vitality</span></div>
                     <div className="text-sm">Empowering Health and wellness through providing accessible community based interventions</div>
-                    <div className="p-2 px-4 w-fit my-4 md:mt-6 lg:mt-8 rounded-l-full rounded-r-full bg-orange-600">Become a Member</div>
+                    <Link href="/join" className="p-2 px-4 w-fit my-4 md:mt-6 lg:mt-8 rounded-l-full rounded-r-full bg-orange-600">Become a Member</Link>
                 </div>
                 <div className="col-span-2 hidden:md:flex flex-col bg-transparent">
                     <Image src="/images/bg.png" alt="Image" width={600} height={600} className="bg-transparent"/>
