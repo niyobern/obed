693f170715b60df880e6ca30783b135d7a8b4115
niyobern <niyobern@outlook.com>
2023-11-27 05:47:16 -0800

    resolved

--- components/navbar.js
+++ components/navbar.js
@@ -5,7 +5,7 @@ export default function NavBar(){
     return (
         <div className="flex justify-between w-full md:w-11/12 lg:w-10/12 mx-auto text-white">
             <Image src={Logo} alt="image" className='w-16 md:w-24 h-10 md:h-16 lg:w-32 lg:h-24'/>
-            <div className="flex gap-20">
+            <div className="hidden md:flex gap-20">
                 <div className="flex gap-2 md:gap-4 lg:gap-6 xl:gap-8">
                     <div>Home</div>
                     <div>About</div>
