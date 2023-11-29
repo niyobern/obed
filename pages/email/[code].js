3d3aec556597e8c807a632073cf5bac265b1e8d7
niyobern <niyobern@outlook.com>
2023-11-28 21:10:33 -0800

    fixed padding

--- pages/email/[code].js
+++ pages/email/[code].js
@@ -25,7 +25,7 @@ function handleSubmit(e){
   return (
     <div className="flex flex-col h-screen bg-gradient-to-b from-[#063970] to-blue-200">
     <div className="grid place-items-center mx-2 my-20 sm:my-auto" x-data="{ showPass: true }">
-        <div className="w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12
+        <div className="w-full p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12
             px-6 py-10 sm:px-10 sm:py-6
             bg-white rounded-lg shadow-md lg:shadow-lg">
             <div className="text-center mb-2">
